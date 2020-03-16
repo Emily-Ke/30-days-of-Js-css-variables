748ce8159941e56e86ad43d3d3ce7dd3ac4c361e
Em <emily.clay.k@gmail.com>
2020-03-16 09:14:10 -0700

    adds prettier

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 const rootElement = document.documentElement;
 const inputElements = document.querySelectorAll('.control-wrapper input');
 
@@ -6,18 +5,25 @@ const defaultValues = {
   'base-color': '#000',
   spacing: '0px',
   blur: '0px'
-}
+};
 
 function setDefaults() {
-  Object.entries(defaultValues).forEach(([key, value]) => 
-  rootElement.style.setProperty(`--${key}`, value));
+  Object.entries(defaultValues).forEach(([key, value]) =>
+    rootElement.style.setProperty(`--${key}`, value)
+  );
 }
 
 function updateCSSVariable(e) {
-  const { dataset: { suffix }, name, value } = e.target;
+  const {
+    dataset: { suffix },
+    name,
+    value
+  } = e.target;
   rootElement.style.setProperty(`--${name}`, `${value}${suffix || ''}`);
 }
 
-inputElements.forEach(inputElement => inputElement.addEventListener('input', updateCSSVariable));
+inputElements.forEach(inputElement =>
+  inputElement.addEventListener('input', updateCSSVariable)
+);
 
 setDefaults();
